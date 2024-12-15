47bac11eb06cfc2949de2fc6e1ade7cb6a082140
John Comeau <jc@unternet.net>
2024-12-15 15:37:33 -0600

    problem starts right around drawing green lines

--- columns.cs
+++ columns.cs
@@ -223,7 +223,7 @@
     % use fontsize as a proxy for moving to baseline. make it better later.
     (stack before top green line: ) #only #stack margin 4 index green hr
     currentfont font.size 4 index exch sub x exch
-    (stack before drawing green line at baseline of headline) #only #stack
+    (stack before drawing green line at baseline of headline: ) #only #stack
     margin 1 index green hr
     moveto headline
     (stack before showing headline: ) #only #stack
