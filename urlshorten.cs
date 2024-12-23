5ca376d5bdc07d5c0eab086341cf69df61210c65
John Comeau <jc@unternet.net>
2024-12-23 09:53:25 -0800

    working on unique links

--- urlshorten.cs
+++ urlshorten.cs
@@ -1,45 +1,55 @@
-#!/usr/local/casperscript/bin/bccs --
+#!/usr/local/casperscript/bin/bccs -S -P --
 /urllib false import
 /urlshorten  ( str#url - str#shorturl bool#true OR str#url bool#false
   generate a short URL for a long one) docstring {
   10 dict begin
   /tries 10 def
-  %dup urllib.quote (../gnixl/gnixl.com/l/) exch string.add dup os.path.exists
-  mark  % make it easy to clean stack regardless of where it failed
-  tries  % sufficient number of tries to find a unique random name
+  /urldomain (gnixl.com) def
+  /urldir (../gnixl/) urldomain string.add def
+  /linkdir (l/) def
+  /linkpath urldir linkdir string.add def
+  /urlprefix urldomain linkdir string.add def
+  dup [] urllib.parse.quote linkdir exch string.add
+  dup os.path.exists (stack before `mark`: ) #only #stack
+    {128 string readlink urlprefix exch add true}
     {
-      {
-        urand 36 10 string cvrs string.lower
-        %(stack after creating directory name: ) #only #stack
-        (../gnixl/gnixl.com/l/) exch string.add dup 8#755
-        %(stack before os.mkdir: ) #only #stack
-        os.mkdir
-        %(stack after os.mkdir: ) #only #stack
-      }
-      stopped
-        {cleartomark mark}
-        {true exit}  % mark success with `true`
-        ifelse
-    }
-    repeat
-  %(after mkdir: ) #only #stack
-  true ne  % failure would leave only a -mark- on stack
-    {(failed after ) #only tries #only ( attempts) # false}
-    {
-      dup (/../../.htaccess) string.add (a) file
-      % chop first part of path to form URL
-      exch (../gnixl/gnixl.com) string.removeprefix
-      %(after forming URL: ) #only #stack
-      (Redirect 301 ) 1 index string.add  % "from" URL added
-      %(first part of redirect: ) #only #stack
-      ( ) string.add 5 -1 roll string.add %#stack
-      (\n) string.add 2 index exch writestring %#stack
-      exch closefile %#stack
-      exch pop  % discard `mark`
-      (gnixl.com) exch string.add  % return as full URL except for scheme://
-      true
+      exch  % put quoted URL out of the way for now
+      mark  % make it easy to clean stack regardless of where it failed
+      tries  % sufficient number of tries to find a unique random name
+        {
+          {
+            urand 36 10 string cvrs string.lower
+            %(stack after creating directory name: ) #only #stack
+            linkdir exch string.add dup 8#755
+            %(stack before os.mkdir: ) #only #stack
+            os.mkdir
+            %(stack after os.mkdir: ) #only #stack
+          }
+          stopped
+            {cleartomark mark}
+            {true exit}  % mark success with `true`
+            ifelse
+        }
+        repeat
+      %(after mkdir: ) #only #stack
+      true ne  % failure would leave only a -mark- on stack
+        {(failed after ) #only tries #only ( attempts) # false}
+        {
+          dup (/../../.htaccess) string.add (a) file
+          % chop first part of path to form URL
+          exch urldir string.removeprefix
+          %(after forming URL: ) #only #stack
+          (Redirect 301 ) 1 index string.add  % "from" URL added
+          %(first part of redirect: ) #only #stack
+          ( ) string.add 5 -1 roll string.add %#stack
+          (\n) string.add 2 index exch writestring %#stack
+          exch closefile %#stack
+          exch pop  % discard `mark`
+          urldomain exch string.add  % return as full URL except for scheme://
+          true
+        } ifelse
+      %(stack after urlshorten: ) #only #stack
     } ifelse
-  %(stack after urlshorten: ) #only #stack
   end
 } bind def
 
